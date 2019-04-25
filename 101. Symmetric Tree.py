ed39e00bd8a01c0e9c110e36ef3551207756ffee
yons <yons@iMac-10.local>
2019-04-25 20:43:54 +0800

    add

--- 101. Symmetric Tree.py
+++ 101. Symmetric Tree.py
@@ -1,21 +1,36 @@
-# Definition for a binary tree node.
-# class TreeNode(object):
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
 
 class Solution(object):
     def isSymmetric(self, root):
-        """
-        :type root: TreeNode
-        :rtype: bool
-        """
-        arr = []
+        dist = None
+        t = self.create(root, dist)
+        return self.isSameTree(root, t)
 
+    def pnt(self, r):
+        if r:
+            print(r.val)
+            self.pnt(r.left)
+            self.pnt(r.right)
 
-    def search(self, r, n):
+    def create(self, source, dist):
+        if source == None:
+            dist = None;
+        else:
+            dist = TreeNode(source.val)
+            dist.right = self.create(source.left, dist.right)
+            dist.left = self.create(source.right, dist.left)
+            return dist
 
+    def isSameTree(self, p, q):
+        if p == None and q == None:
+            return True
 
-        pass
+        if p == None or q == None:
+            return False
 
+        if p.val != q.val:
+            return False
+
+        sl = self.isSameTree(p.left, q.left)
+        sr = self.isSameTree(p.right, q.right)
+
+        return sl and sr
